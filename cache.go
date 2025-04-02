f9c13e491a6062954b60e425faf344367111cb16
Takumi Kanada <candy12t@gmail.com>
2025-04-02 22:49:42 +0900

    with ttl

--- cache.go
+++ cache.go
@@ -2,32 +2,57 @@ package cache
 
 import (
 	"sync"
+	"time"
 )
+
+type CacheItem struct {
+	value  any
+	expiry time.Time
+}
 
 type Cache struct {
 	mu   sync.RWMutex
-	data map[string]any
+	data map[string]CacheItem
 }
 
 func NewCache() *Cache {
 	return &Cache{
-		data: make(map[string]any),
+		data: make(map[string]CacheItem),
 	}
 }
 
-func (c *Cache) Set(key string, value any) {
+func (c *Cache) Set(key string, value any, ttl time.Duration) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	c.data[key] = value
+	var expiry time.Time
+	if ttl > 0 {
+		expiry = time.Now().Add(ttl)
+	} else {
+		expiry = time.Time{}
+	}
+
+	c.data[key] = CacheItem{
+		value:  value,
+		expiry: expiry,
+	}
 }
 
 func (c *Cache) Get(key string) (any, bool) {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
-	value, ok := c.data[key]
-	return value, ok
+	item, ok := c.data[key]
+	if !ok {
+		return nil, false
+	}
+
+	if !item.expiry.IsZero() && item.expiry.Before(time.Now()) {
+		delete(c.data, key)
+		return nil, false
+	}
+
+	return item.value, ok
 }
 
 func (c *Cache) Delete(key string) {
@@ -41,5 +66,5 @@ func (c *Cache) Flush() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	c.data = make(map[string]any)
+	c.data = make(map[string]CacheItem)
 }
